35718d3ba3ceadb15197cc140b89edfa34cb0e64
LiveWithMike <mikevanderputten@outlook.com>
2024-01-19 21:34:23 +0100

    Ambulance added

---
+++ ambulance/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+
+    'data/**/vehicles.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+
+pmc_updates 'yes'
+pmc_github 'https://github.com/SASVegas/SASVEGAS'
+pmc_version '1.0'

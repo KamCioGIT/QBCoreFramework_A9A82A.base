aefac7c710f7326d43cb71c13b5effb003c261db
LiveWithMike <mikevanderputten@outlook.com>
2024-01-19 21:44:50 +0100

    Some removed, some updated

--- [overige]/phone/apps/banking/cl_banking.lua
+++
@@ -1,9 +0,0 @@
-RegisterNUICallback("getBank", function(data, cb)
-  local bank = Phone.API.TriggerServerEvent('bank:get')
-  cb({bank = bank})
-end)
-
-RegisterNUICallback("transfer", function(data, cb)
-  Phone.API.TriggerServerEvent('bank:transfer', data.id, tonumber(data.amt))
-  cb("OK")
-end)
--- [overige]/phone/web/src/lib/sound.ts
+++
@@ -1,6 +0,0 @@
-export const playSound = (file: string, volume?: any) => {
-  const audio = new Audio(`./sounds/${file}`);
-  audio.volume = volume ? volume : 0.3;
-  audio.play();
-  return audio;
-};

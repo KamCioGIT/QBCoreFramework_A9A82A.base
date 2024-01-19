8d4c2a2fa46b4d968c6d8cbafecf35b20bc3ec5b
LiveWithMike <mikevanderputten@outlook.com>
2024-01-19 19:03:09 +0100

    Update fxmanifest.lua

--- PLOKS_CARS/fxmanifest.lua
+++ PLOKS_CARS/fxmanifest.lua
@@ -17,7 +17,6 @@ files {
     'data/**/*.ytyp'
 }
 
-
 data_file 'HANDLING_FILE'            'data/**/handling*.meta'
 data_file 'VEHICLE_LAYOUTS_FILE'    'data/**/vehiclelayouts*.meta'
 data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles*.meta'

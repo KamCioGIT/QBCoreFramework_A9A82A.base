a4f1009d9ed62d40613f63ba78179943a6d84aad
LiveWithMike <mikevanderputten@outlook.com>
2024-02-07 13:24:49 +0100

    Latest Version

---
+++ resources/[autos]/motoren/fxmanifest.lua
@@ -0,0 +1,16 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'data/**/*.meta',
+}
+
+
+data_file 'HANDLING_FILE'            'data/**/handling*.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles*.meta'
+data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
+
+
+client_script "@Badger-Anticheat/acloader.lua"

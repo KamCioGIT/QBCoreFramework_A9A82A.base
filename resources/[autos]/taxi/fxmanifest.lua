c6a4930aa6e52a5efe529be5fa53ec23fcc75c64
LiveWithMike <mikevanderputten@outlook.com>
2024-02-07 13:33:46 +0100

    Latest Version

---
+++ resources/[autos]/taxi/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'data/carvariations.meta',
+}
+
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+pmc_updates 'yes'
+pmc_github 'https://github.com/SASVegas/SASVEGAS/tree/main/taxi'
+pmc_version '1.0'
+client_script "@Badger-Anticheat/acloader.lua"

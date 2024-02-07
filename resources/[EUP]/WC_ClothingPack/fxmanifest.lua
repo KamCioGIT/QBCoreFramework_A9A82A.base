6291cf74e2e1c67b075804152dbdf7f92a66deca
LiveWithMike <mikevanderputten@outlook.com>
2024-02-07 14:07:19 +0100

    Latest Version

---
+++ resources/[EUP]/WC_ClothingPack/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_busmods.meta',
+  'mp_f_freemode_01_mp_f_busmods.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_busmods.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_busmods.meta'
+
+client_script "@Badger-Anticheat/acloader.lua"
